949e3cd6fda936f3f00c54bd67234555a2738346
jhennyfloresm <jfloresm2@fcpn.edu.bo>
2025-03-24 00:33:59 -0400

    Update EcuacionCuadratica_modular.java

--- Practica 5/Ejercicio1/EcuacionCuadratica_modular.java
+++ Practica 5/Ejercicio1/EcuacionCuadratica_modular.java
@@ -1,24 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author WINDOWS 11
- */
-
-
-/**
- *
- * @author WINDOWS 11
- * 
- */
-
-
 public class EcuacionCuadratica_modular {
 
     
@@ -50,9 +32,6 @@ public class EcuacionCuadratica_modular {
             System.out.println("La ecuación no tiene raíces reales.");
         }
     }
-
-    
-  
 
   public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
