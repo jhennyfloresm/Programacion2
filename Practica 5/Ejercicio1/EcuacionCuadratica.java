4432b82fa08cd9ac8644b940df64e4f9955b2668
jhennyfloresm <jfloresm2@fcpn.edu.bo>
2025-03-24 00:30:37 -0400

    Add files via upload

---
+++ Practica 5/Ejercicio1/EcuacionCuadratica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecuacioncuadraticpoo;
+
+/**
+ *
+ * @author WINDOWS 11
+ */
+public class EcuacionCuadratica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
